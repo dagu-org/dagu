278d09a1370a78b073e19ea28f513772789de769
yohamta <yohamta@gmail.com>
2022-06-24 14:38:50 +0900

    feat: web UI table row grouping

--- admin/src/components/DAGTable.tsx
+++ admin/src/components/DAGTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { HTMLProps } from "react";
 import {
   createTable,
   useTableInstance,
@@ -7,6 +7,9 @@ import {
   SortingState,
   getFilteredRowModel,
   ColumnFiltersState,
+  Row,
+  ExpandedState,
+  getExpandedRowModel,
 } from "@tanstack/react-table";
 import DAGActions from "./DAGActions";
 import StatusChip from "./StatusChip";
@@ -14,6 +17,7 @@ import {
   Autocomplete,
   Box,
   Chip,
+  IconButton,
   Stack,
   Table,
   TableBody,
@@ -30,9 +34,9 @@ import {
   getStatusField,
   DAGItem,
   DAGDataType,
-  DAGGroup,
 } from "../models/DAGData";
 import StyledTableRow from "./StyledTableRow";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 
 type Props = {
   DAGs: DAGItem[];
@@ -40,38 +44,72 @@ type Props = {
   refreshFn: () => Promise<void>;
 };
 
+type DAGRow = DAGItem & { subRows?: DAGItem[] };
+
 const table = createTable()
-  .setRowType<DAGItem>()
-  .setFilterMetaType<DAGItem>()
+  .setRowType<DAGRow>()
+  .setFilterMetaType<DAGRow>()
   .setTableMetaType<{
     group: string;
     refreshFn: () => Promise<void>;
   }>();
 
-const UpperGroup = "../";
-
 const defaultColumns = [
   table.createDataColumn("Name", {
-    id: "DAG",
-    header: "DAG",
-    cell: (props) => {
-      const data = props.row.original!;
+    id: "Expand",
+    header: ({ instance }) => {
+      return (
+        <IconButton onClick={instance.getToggleAllRowsExpandedHandler()}>
+          {instance.getIsAllRowsExpanded() ? (
+            <KeyboardArrowUp />
+          ) : (
+            <KeyboardArrowDown />
+          )}
+        </IconButton>
+      );
+    },
+    cell: ({ row }) => {
+      if (row.getCanExpand()) {
+        return (
+          <IconButton onClick={row.getToggleExpandedHandler()}>
+            {row.getIsExpanded() ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+          </IconButton>
+        );
+      }
+      return "";
+    },
+    enableSorting: false,
+  }),
+  table.createDataColumn("Name", {
+    id: "Name",
+    cell: ({ row, getValue }) => {
+      const data = row.original!;
       if (data.Type == DAGDataType.Group) {
-        if (data.Name == UpperGroup) {
-          return <Link to={`/dags`}>{props.getValue()}</Link>;
-        } else {
-          return (
-            <Link to={`/dags/?group=${encodeURI(data.Name)}`}>
-              {props.getValue()}
-            </Link>
-          );
-        }
+        return getValue();
       } else {
         const name = data.DAG.File.replace(/.y[a]{0,1}ml$/, "");
-        const group = props.instance.options.meta?.group || "";
         const url = `/dags/${encodeURI(name)}`;
-        return <Link to={url}>{props.getValue()}</Link>;
+        return (
+          <div
+            style={{
+              paddingLeft: `${row.depth * 2}rem`,
+            }}
+          >
+            <Link to={url}>{getValue()}</Link>
+          </div>
+        );
       }
+    },
+    filterFn: (props, _, filter) => {
+      const data = props.original!;
+      let value = "";
+      if (data.Type == DAGDataType.Group) {
+        value = data.Name;
+      } else {
+        value = data.DAG.Config.Name;
+      }
+      const ret = value.toLowerCase().includes(filter.toLowerCase());
+      return ret;
     },
   }),
   table.createDataColumn("Type", {
@@ -225,7 +263,6 @@ function DAGTable({ DAGs = [], group = "", refreshFn }: Props) {
     []
   );
   const [sorting, setSorting] = React.useState<SortingState>([]);
-  const [globalFilter, setGlobalFilter] = React.useState("");
 
   const selectedTag = React.useMemo(() => {
     return (
@@ -234,41 +271,45 @@ function DAGTable({ DAGs = [], group = "", refreshFn }: Props) {
     );
   }, [columnFilters]);
 
+  const [expanded, setExpanded] = React.useState<ExpandedState>({});
+
   const data = React.useMemo(() => {
-    const ret: DAGItem[] = [];
     const groups: {
-      [key: string]: DAGGroup;
+      [key: string]: DAGRow;
     } = {};
     DAGs.forEach((dag) => {
       if (dag.Type == DAGDataType.DAG) {
-        if (dag.DAG.Config.Group == group) {
-          ret.push(dag);
-        } else if (group == "") {
-          const group = dag.DAG.Config.Group;
-          if (!groups[group]) {
-            groups[group] = {
+        const g = dag.DAG.Config.Group;
+        if (g != "") {
+          if (!groups[g]) {
+            groups[g] = {
               Type: DAGDataType.Group,
-              Name: group,
-              DAGs: [],
+              Name: g,
+              subRows: [],
             };
           }
-          groups[group].DAGs.push(dag);
+          groups[g].subRows!.push(dag);
         }
       }
     });
-    if (group != "") {
-      groups[UpperGroup] = {
-        Type: DAGDataType.Group,
-        Name: UpperGroup,
-        DAGs: [],
-      };
-    }
+    const ret: DAGRow[] = [];
     const groupKeys = Object.keys(groups);
-    return [...groupKeys.map((k) => groups[k]), ...ret];
+    groupKeys.forEach((key) => {
+      ret.push(groups[key]);
+    });
+    return [
+      ...ret,
+      ...DAGs.filter(
+        (dag) =>
+          dag.Type == DAGDataType.DAG &&
+          dag.DAG.Config.Group == "" &&
+          dag.DAG.Config.Group == group
+      ),
+    ];
   }, [DAGs, group]);
 
   const tagOptions = React.useMemo(() => {
-    const map: { [key: string]: boolean } = {};
+    const map: { [key: string]: boolean } = { "": true };
     DAGs.forEach((data) => {
       if (data.Type == DAGDataType.DAG) {
         data.DAG.Config.Tags?.forEach((tag) => {
@@ -283,41 +324,21 @@ function DAGTable({ DAGs = [], group = "", refreshFn }: Props) {
   const instance = useTableInstance(table, {
     data,
     columns,
+    getSubRows: (row) => row.subRows,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
-    onGlobalFilterChange: setGlobalFilter,
     onColumnFiltersChange: setColumnFilters,
-    globalFilterFn: (row, _, globalFilter) => {
-      const data = row.original;
-      if (!data) {
-        return false;
-      }
-      if (data.Type == DAGDataType.Group) {
-        return data.Name.toLowerCase().includes(globalFilter);
-      }
-      const DAG = data.DAG;
-      if (DAG.Config.Name.toLowerCase().includes(globalFilter)) {
-        return true;
-      }
-      if (DAG.Config.Description.toLowerCase().includes(globalFilter)) {
-        return true;
-      }
-      const tags = DAG.Config?.Tags;
-      if (
-        tags &&
-        tags.some((tag) => tag.toLowerCase().includes(globalFilter))
-      ) {
-        return true;
-      }
-      return false;
-    },
+    getExpandedRowModel: getExpandedRowModel(),
+    autoResetExpanded: false,
     state: {
       sorting,
-      globalFilter,
+      expanded,
       columnFilters,
     },
     onSortingChange: setSorting,
+    onExpandedChange: setExpanded,
+    debugAll: true,
     meta: {
       group,
       refreshFn,
@@ -338,10 +359,9 @@ function DAGTable({ DAGs = [], group = "", refreshFn }: Props) {
           label="Search Text"
           size="small"
           InputProps={{
-            value: globalFilter || "",
-            onChange: (value) => {
-              const data = value.target.value;
-              setGlobalFilter(data);
+            value: instance.getColumn("Name").getFilterValue(),
+            onChange: (e) => {
+              instance.getColumn("Name").setFilterValue(e.target.value || "");
             },
             type: "search",
           }}
@@ -363,23 +383,37 @@ function DAGTable({ DAGs = [], group = "", refreshFn }: Props) {
           {instance.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
-                <TableCell key={header.id} colSpan={header.colSpan}>
-                  <Box
-                    {...{
-                      sx: {
-                        cursor: header.column.getCanSort()
-                          ? "pointer"
-                          : "default",
-                      },
-                      onClick: header.column.getToggleSortingHandler(),
-                    }}
-                  >
-                    {header.isPlaceholder ? null : header.renderHeader()}
-                    {{
-                      asc: <TableSortLabel direction="asc" />,
-                      desc: <TableSortLabel direction="desc" />,
-                    }[header.column.getIsSorted() as string] ?? null}
-                  </Box>
+                <TableCell
+                  key={header.id}
+                  style={{
+                    padding:
+                      header.id == "Expand" || header.id == "Name"
+                        ? "6px 4px"
+                        : "6px 16px",
+                  }}
+                >
+                  {header.column.getCanSort() ? (
+                    <Stack direction="column" spacing={1}>
+                      <Box
+                        {...{
+                          sx: {
+                            cursor: header.column.getCanSort()
+                              ? "pointer"
+                              : "default",
+                          },
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {header.isPlaceholder ? null : header.renderHeader()}
+                        {{
+                          asc: <TableSortLabel direction="asc" />,
+                          desc: <TableSortLabel direction="desc" />,
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </Box>
+                    </Stack>
+                  ) : (
+                    header.renderHeader()
+                  )}
                 </TableCell>
               ))}
             </TableRow>
@@ -387,14 +421,20 @@ function DAGTable({ DAGs = [], group = "", refreshFn }: Props) {
         </TableHead>
         <TableBody>
           {instance.getRowModel().rows.map((row) => (
-            <StyledTableRow
-              key={row.id}
-              style={{
-                height: "50px",
-              }}
-            >
+            <StyledTableRow key={row.id} style={{ height: "44px" }}>
               {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id}>{cell.renderCell()}</TableCell>
+                <TableCell
+                  key={cell.id}
+                  style={{
+                    padding:
+                      cell.column.id == "Expand" || cell.column.id == "Name"
+                        ? "6px 4px"
+                        : "6px 16px",
+                  }}
+                  width={cell.column.id == "Expand" ? "44px" : undefined}
+                >
+                  {cell.renderCell()}
+                </TableCell>
               ))}
             </StyledTableRow>
           ))}
