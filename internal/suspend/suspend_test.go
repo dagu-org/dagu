cd3d331506e6a14a87f543f954b27c9b3589052d
yohamta <yohamta@gmail.com>
2022-07-12 20:31:08 +0900

    feat: added suspend package

---
+++ internal/suspend/suspend_test.go
@@ -0,0 +1,39 @@
+package suspend
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/yohamta/dagu/internal/config"
+	"github.com/yohamta/dagu/internal/storage"
+	"github.com/yohamta/dagu/internal/utils"
+)
+
+func TestSuspendChecker(t *testing.T) {
+	tmpDir := utils.MustTempDir("test-suspend-checker")
+	defer os.RemoveAll(tmpDir)
+
+	s := storage.NewStorage(tmpDir)
+
+	sc := NewSuspendChecker(s)
+
+	cfg := &config.Config{
+		Name: "test",
+	}
+
+	suspend := sc.IsSuspended(cfg)
+	require.False(t, suspend)
+
+	err := sc.ToggleSuspend(cfg, true)
+	require.NoError(t, err)
+
+	suspend = sc.IsSuspended(cfg)
+	require.True(t, suspend)
+
+	err = sc.ToggleSuspend(cfg, false)
+	require.NoError(t, err)
+
+	suspend = sc.IsSuspended(cfg)
+	require.False(t, suspend)
+}
