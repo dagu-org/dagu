18f88f435b9bd8b6b2871cdee53abbb8cddd760e
yohamta <yohamta@gmail.com>
2022-07-12 20:04:19 +0900

    feat: storage package

---
+++ internal/storage/storage.go
@@ -0,0 +1,35 @@
+package storage
+
+import (
+	"os"
+	"path"
+)
+
+// Storage is a storage for flags.
+type Storage struct {
+	Dir string
+}
+
+// NewStorage creates a new storage.
+func NewStorage(dir string) *Storage {
+	os.MkdirAll(dir, 0755)
+	return &Storage{
+		Dir: dir,
+	}
+}
+
+// Create creates the given file.
+func (s *Storage) Create(file string) error {
+	return os.WriteFile(path.Join(s.Dir, file), []byte{}, 0644)
+}
+
+// Exists returns true if the given file exists.
+func (s *Storage) Exists(file string) bool {
+	_, err := os.Stat(path.Join(s.Dir, file))
+	return err == nil
+}
+
+// Delete deletes the given file.
+func (s *Storage) Delete(file string) error {
+	return os.Remove(path.Join(s.Dir, file))
+}
---
+++ internal/storage/storage_test.go
@@ -0,0 +1,33 @@
+package storage
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/yohamta/dagu/internal/utils"
+)
+
+func TestStorage(t *testing.T) {
+	tmpDir := utils.MustTempDir("test-storage")
+	defer os.RemoveAll(tmpDir)
+
+	s := NewStorage(tmpDir)
+
+	f := "test.flag"
+	exist := s.Exists(f)
+	require.False(t, exist)
+
+	err := s.Create(f)
+	require.NoError(t, err)
+
+	exist = s.Exists(f)
+	require.True(t, exist)
+
+	err = s.Delete(f)
+	require.NoError(t, err)
+
+	exist = s.Exists(f)
+	require.False(t, exist)
+}
