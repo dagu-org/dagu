2f0cde6f1297561d5e6f59285fca2b9e12d10473
yohamta <yohamta@gmail.com>
2022-07-13 20:37:29 +0900

    feat: display log file information on web UI

--- admin/src/components/DAGLog.tsx
+++ admin/src/components/DAGLog.tsx
@@ -1,8 +1,10 @@
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import React from 'react';
 import { LogFile } from '../api/DAG';
 import BorderedBox from './BorderedBox';
+import LabeledItem from './LabeledItem';
 import Loading from './Loading';
+import NodeStatusChip from './NodeStatusChip';
 import SubTitle from './SubTitle';
 
 type Props = {
@@ -16,14 +18,31 @@ function DAGLog({ log }: Props) {
   return (
     <Box>
       <SubTitle>Log</SubTitle>
+      <Stack spacing={1} direction="column" sx={{ width: '100%' }}>
+        <LabeledItem label="Log File">{log.LogFile}</LabeledItem>
+        {log.Step ? (
+          <React.Fragment>
+            <LabeledItem label="Step Name">{log.Step.Step.Name}</LabeledItem>
+            <Stack spacing={2} direction="row" sx={{ alignItems: 'center' }}>
+              <LabeledItem label="Started At">{log.Step.StartedAt}</LabeledItem>
+              <LabeledItem label="Finished At">
+                {log.Step.FinishedAt}
+              </LabeledItem>
+            </Stack>
+            <LabeledItem label="Status">
+              <NodeStatusChip status={log.Step.Status}>
+                {log.Step.StatusText}
+              </NodeStatusChip>
+            </LabeledItem>
+          </React.Fragment>
+        ) : null}
+      </Stack>
       <BorderedBox
         sx={{
           mt: 2,
           py: 2,
           px: 2,
-          display: 'flex',
-          flexDirection: 'column',
-          height: '70vh',
+          height: '60vh',
           overflow: 'auto',
         }}
       >
