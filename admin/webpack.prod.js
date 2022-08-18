31ceacd30e37ba61af2c85712dc7c74725540752
yohamta <yohamta@gmail.com>
2022-08-18 20:05:43 +0900

    formatter

--- admin/webpack.prod.js
+++ admin/webpack.prod.js
@@ -1,20 +1,20 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const path = require("path");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: [
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
           },
         ],
-        include: path.resolve(__dirname, "src"),
-        exclude: path.resolve(__dirname, "node_modules"),
+        include: path.resolve(__dirname, 'src'),
+        exclude: path.resolve(__dirname, 'node_modules'),
       },
     ],
   },
