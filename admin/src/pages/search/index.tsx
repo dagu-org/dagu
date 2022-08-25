210efd2fdebbd79a824e635a17f37457b7d8fe8b
yohamta <yohamta@gmail.com>
2022-08-25 16:53:24 +0900

    admin-web: search page

---
+++ admin/src/pages/search/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef } from 'react';
+import { Box, Button, Grid, Stack, TextField } from '@mui/material';
+import { AppBarContext } from '../../contexts/AppBarContext';
+import useSWR from 'swr';
+import Title from '../../components/atoms/Title';
+
+function Search() {
+  const appBarContext = React.useContext(AppBarContext);
+  const [searchVal, setSearchVal] = React.useState('');
+
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    ref.current?.focus();
+  }, [ref.current]);
+
+  return (
+    <Grid container spacing={3} sx={{ mx: 4, width: '100%' }}>
+      <Grid item xs={12}>
+        <Title>Search</Title>
+        <Stack spacing={2} direction="row">
+          <TextField
+            variant="outlined"
+            style={{
+              flex: 0.5,
+            }}
+            inputRef={ref}
+            InputProps={{
+              value: searchVal,
+              onChange: (e) => {
+                setSearchVal(e.target.value);
+              },
+              type: 'search',
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                console.log('submit');
+              }
+            }}
+          />
+          <Button
+            variant="contained"
+            sx={{
+              width: '100px',
+              border: 0,
+            }}
+            onClick={async () => {}}
+          >
+            Search
+          </Button>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
+}
+export default Search;
