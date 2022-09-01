2536b9ccd2dfb3c69e312ed8ab8c173915ba812b
yohamta <yohamta@gmail.com>
2022-09-01 12:56:59 +0900

    admin-web: add delete DAG button

--- admin/src/components/molecules/DAGEditButtons.tsx
+++ admin/src/components/molecules/DAGEditButtons.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@mui/material';
 import React from 'react';
+import { Button, Stack } from '@mui/material';
 
 type Props = {
   name: string;
@@ -7,35 +7,57 @@ type Props = {
 
 function DAGEditButtons({ name }: Props) {
   return (
-    <Button
-      onClick={async () => {
-        const val = window.prompt('Please input the new DAG name', '');
-        if (!val) {
-          return;
-        }
-        if (val.indexOf(' ') != -1) {
-          alert('DAG name cannot contain space');
-          return;
-        }
-        const formData = new FormData();
-        formData.append('action', 'rename');
-        formData.append('value', val);
-        const url = `${API_URL}/dags/${name}`;
-        const resp = await fetch(url, {
-          method: 'POST',
-          headers: { Accept: 'application/json' },
-          body: formData,
-        });
-        if (resp.ok) {
-          window.location.href = `/dags/${val}`;
-        } else {
-          const e = await resp.text();
-          alert(e);
-        }
-      }}
-    >
-      Rename
-    </Button>
+    <Stack direction="row" spacing={1}>
+      <Button
+        onClick={async () => {
+          const val = window.prompt('Please input the new DAG name', '');
+          if (!val) {
+            return;
+          }
+          if (val.indexOf(' ') != -1) {
+            alert('DAG name cannot contain space');
+            return;
+          }
+          const formData = new FormData();
+          formData.append('action', 'rename');
+          formData.append('value', val);
+          const url = `${API_URL}/dags/${name}`;
+          const resp = await fetch(url, {
+            method: 'POST',
+            headers: { Accept: 'application/json' },
+            body: formData,
+          });
+          if (resp.ok) {
+            window.location.href = `/dags/${val}`;
+          } else {
+            const e = await resp.text();
+            alert(e);
+          }
+        }}
+      >
+        Rename
+      </Button>
+      <Button
+        onClick={async () => {
+          if (!confirm('Are you sure to delete the DAG?')) {
+            return;
+          }
+          const url = `${API_URL}/dags/${name}`;
+          const resp = await fetch(url, {
+            method: 'DELETE',
+            headers: { Accept: 'application/json' },
+          });
+          if (resp.ok) {
+            window.location.href = '/dags/';
+          } else {
+            const e = await resp.text();
+            alert(e);
+          }
+        }}
+      >
+        Delete
+      </Button>
+    </Stack>
   );
 }
 
