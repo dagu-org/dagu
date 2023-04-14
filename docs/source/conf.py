4048ff2b2bf6f06f3dc3b35c9bdf2a1c62de3d63
Yota Hamada <yohamta@gmail.com>
2023-04-14 22:05:42 +0900

    add translation files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,3 +26,7 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# -- Options for Localization ------------------------------------------------
+locale_dirs = ['locale/']  
+gettext_compact = False  
