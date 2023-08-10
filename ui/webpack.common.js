aad9fd34e154393fa0e0bc1289971d2d8bcffa3b
yohamta <yohamta@gmail.com>
2023-08-10 21:22:46 +0900

    add YAML editor support

--- ui/webpack.common.js
+++ ui/webpack.common.js
@@ -1,46 +1,54 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|gif)$/i,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
-              encoding: "base64",
+              encoding: 'base64',
             },
           },
         ],
       },
-			{
-				test: /\.ttf$/,
-				type: 'asset/resource'
-			}
+      {
+        test: /\.ttf$/,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
-    new MonacoWebpackPlugin(
-      {
-        languages: ["yaml"],
-        features: ["find"]
-      }
-    ),
+    new MonacoWebpackPlugin({
+      languages: ['yaml'],
+      features: ['find'],
+      customLanguages: [
+        {
+          label: 'yaml',
+          entry: 'monaco-yaml',
+          worker: {
+            id: 'monaco-yaml/yamlWorker',
+            entry: 'monaco-yaml/yaml.worker',
+          },
+        },
+      ],
+    }),
   ],
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     clean: true,
   },
 };
