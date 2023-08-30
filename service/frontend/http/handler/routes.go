f8e4a4ee16c1b1b4ae7f7df66fad45c52f1c15d8
Yota Hamada <yohamta@gmail.com>
2023-08-30 23:03:22 +0900

    refactor frontend api

--- service/frontend/http/handler/routes.go
+++ service/frontend/http/handler/routes.go
@@ -1,44 +1,27 @@
 package handlers
 
 import (
-	"encoding/json"
 	"github.com/go-chi/chi/v5"
-	"log"
 	"net/http"
 )
 
 func ConfigRoutes(r *chi.Mux) *chi.Mux {
-	r.Get("/", handleIndex())
-
-	r.Route("/dags", func(r chi.Router) {
-		r.Get("/", handleIndex())
-
-		dagRoute := func(r chi.Router) {
-			r.Get("/", handleGetDAG())
-		}
-
-		r.Route("/{dagName}", dagRoute)
-		r.Route("/{dagName}/{tabName}", dagRoute)
-	})
-
-	r.Get("/search", handleGetSearch())
 	r.Get("/assets/*", handleGetAssets())
+	r.Get("/*", handleRequest())
 
 	return r
+}
+
+func handleRequest() http.HandlerFunc {
+	renderFunc := useTemplate("index.gohtml", "index")
+	return func(w http.ResponseWriter, r *http.Request) {
+		renderFunc(w, nil)
+	}
 }
 
 func handleGetAssets() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "max-age=86400")
 		http.FileServer(http.FS(assetsFS)).ServeHTTP(w, r)
-	}
-}
-
-func renderJson(w http.ResponseWriter, data interface{}) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	err := json.NewEncoder(w).Encode(data)
-	if err != nil {
-		log.Printf("%v", err)
 	}
 }
