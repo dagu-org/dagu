afce2240d6d0606509c61d2f2dffa0ce5d8d6a88
Yota Hamada <yohamta@gmail.com>
2024-01-01 21:31:03 +0900

    fix basic auth middleware

--- service/frontend/middleware/token_auth.go
+++ service/frontend/middleware/token_auth.go
@@ -11,6 +11,11 @@ import (
 func TokenAuth(realm string, token string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if skipTokenAuth(*r) {
+				next.ServeHTTP(w, r)
+				return
+			}
+
 			authHeader := strings.Split(r.Header.Get("Authorization"), " ")
 			if len(authHeader) < 2 {
 				tokenAuthFailed(w, realm)
@@ -32,6 +37,10 @@ func TokenAuth(realm string, token string) func(next http.Handler) http.Handler 
 
 		})
 	}
+}
+
+func skipTokenAuth(r http.Request) bool {
+	return isAuthenticated(r.Context())
 }
 
 func tokenAuthFailed(w http.ResponseWriter, realm string) {
