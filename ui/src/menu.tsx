0ff09ce0f64582573aeec2351b5431b0dc59b67d
mahmut_erdem.ozgen <mahmut_erdem.ozgen@cr14.net>
2024-02-13 15:41:23 +0300

    Add favicon

--- ui/src/menu.tsx
+++ ui/src/menu.tsx
@@ -2,17 +2,15 @@ import * as React from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import { Link } from 'react-router-dom';
-import { Typography } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faChartGantt,
   faMagnifyingGlass,
   faTableList,
   faTerminal,
-  faUserNinja,
 } from '@fortawesome/free-solid-svg-icons';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { Typography } from '@mui/material';
 
 function Icon({ icon }: { icon: IconProp }) {
   return (
@@ -34,33 +32,42 @@ function Icon({ icon }: { icon: IconProp }) {
 
 export const mainListItems = (
   <React.Fragment>
-    <Link to="/dashboard">
-      <ListItem text="Dashboard" icon={<Icon icon={faChartGantt}></Icon>} />
-    </Link>
-    <Link to="/dags">
-      <ListItem text="DAGs" icon={<Icon icon={faTableList}></Icon>} />
-    </Link>
-    <Link to="/search">
-      <ListItem text="Search" icon={<Icon icon={faMagnifyingGlass}></Icon>} />
-    </Link>
-    <Link to="http://localhost:8090">
-      <ListItem text="Terminal" icon={<Icon icon={faTerminal}></Icon>} />
-    </Link>
-    <Link to="https://localhost:8090">
-      <ListItem text="Terminal" icon={<Icon icon={faUserNinja}></Icon>} />
-    </Link>
-
+    <ListItem
+      text="Dashboard"
+      icon={<Icon icon={faChartGantt} />}
+      to="/dashboard"
+    />
+    <ListItem
+      text="DAGs"
+      icon={<Icon icon={faTableList} />}
+      to="/dags"
+    />
+    <ListItem
+      text="Search"
+      icon={<Icon icon={faMagnifyingGlass} />}
+      to="/search"
+    />
+    <ListItem
+      text="Terminal"
+      icon={<Icon icon={faTerminal} />}
+      to="http://localhost:8090"
+      external
+    />
   </React.Fragment>
 );
 
 type ListItemProps = {
   icon: React.ReactNode;
   text: string;
+  to: string;
+  external?: boolean;
 };
 
-function ListItem({ icon, text }: ListItemProps) {
+function ListItem({ icon, text, to, external }: ListItemProps) {
+  const listItemProps = external ? { href: to, target: '_blank' } : { to };
+
   return (
-    <ListItemButton>
+    <ListItemButton component="a" {...listItemProps}>
       <ListItemIcon sx={{ color: 'black' }}>{icon}</ListItemIcon>
       <ListItemText
         primary={
