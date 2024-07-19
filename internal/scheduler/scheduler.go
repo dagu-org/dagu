3a9b122ec8fa4e8fde3ac6fede8f85ac67d23d6f
Yota Hamada <yohamta@gmail.com>
2024-07-19 22:34:14 +0900

    Refactor scheduler module

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -11,7 +11,9 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/dagu-dev/dagu/internal/config"
 	"github.com/dagu-dev/dagu/internal/dag"
+	"github.com/dagu-dev/dagu/internal/engine"
 	"github.com/dagu-dev/dagu/internal/logger"
 )
 
@@ -21,6 +23,23 @@ type Scheduler struct {
 	stop        chan struct{}
 	running     atomic.Bool
 	logger      logger.Logger
+}
+
+func New(cfg *config.Config, lg logger.Logger, eng engine.Engine) *Scheduler {
+	return newScheduler(newSchedulerArgs{
+		EntryReader: newEntryReader(newEntryReaderArgs{
+			Engine:  eng,
+			DagsDir: cfg.DAGs,
+			JobCreator: &jobCreatorImpl{
+				WorkDir:    cfg.WorkDir,
+				Engine:     eng,
+				Executable: cfg.Executable,
+			},
+			Logger: lg,
+		}),
+		Logger: lg,
+		LogDir: cfg.LogDir,
+	})
 }
 
 type entryReader interface {
