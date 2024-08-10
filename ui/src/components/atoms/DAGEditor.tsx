2a79def33c44e05db57b279f54fded8a707159a4
vhespanha <vhespanha@tuta.io>
2024-08-10 16:52:08 -0300

    Validate dag schema from json file

--- ui/src/components/atoms/DAGEditor.tsx
+++ ui/src/components/atoms/DAGEditor.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import MonacoEditor from 'react-monaco-editor';
+import * as monaco from 'monaco-editor';
+import { setDiagnosticsOptions } from 'monaco-yaml';
 
 type Props = {
   value: string;
@@ -7,12 +9,44 @@ type Props = {
 };
 
 function DAGEditor({ value, onChange }: Props) {
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+  useEffect(() => {
+    setDiagnosticsOptions({
+      enableSchemaRequest: true,
+      schemas: [
+        {
+          uri: 'https://raw.githubusercontent.com/daguflow/dagu/main/schemas/dag.schema.json',
+          fileMatch: ['*'],
+        },
+      ],
+    });
+
+    // cleanup
+    return () => {
+      if (editorRef.current) {
+        editorRef.current.dispose();
+      }
+    };
+  }, []);
+
+  const editorDidMount = (editor: monaco.editor.IStandaloneCodeEditor) => {
+    editorRef.current = editor;
+  };
+
   return (
     <MonacoEditor
       height="60vh"
+      language="yaml"
       value={value}
       onChange={onChange}
-      language="yaml"
+      editorDidMount={editorDidMount}
+      options={{
+        automaticLayout: true,
+        scrollBeyondLastLine: false,
+        quickSuggestions: { other: true, comments: false, strings: true },
+        formatOnType: true,
+      }}
     />
   );
 }
