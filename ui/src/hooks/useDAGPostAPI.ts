6a0c52289210f4651f717586b96fc6d5e2e1c151
Yota Hamada <yohamta@gmail.com>
2025-02-01 22:49:51 +0900

    fix bug

--- ui/src/hooks/useDAGPostAPI.ts
+++ ui/src/hooks/useDAGPostAPI.ts
@@ -10,33 +10,31 @@ type Options = {
 
 export function useDAGPostAPI(opts: Options) {
   const appBarContext = React.useContext(AppBarContext);
-  const doPost = React.useCallback(
-    async (action: string, step?: string) => {
-      const url = `${getConfig().apiURL}/dags/${opts.name}?remoteNode=${
-        appBarContext.selectedRemoteNode || 'local'
-      }`;
-      const ret = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          action: action,
-          step: step,
-          requestId: opts.requestId,
-        }),
-      });
-      if (ret.ok) {
-        opts?.onSuccess?.();
-      } else {
-        const e = await ret.text();
-        alert(e);
-        opts?.onFailed?.();
-      }
-    },
-    [opts]
-  );
+  const doPost = async (action: string, step?: string) => {
+    const url = `${getConfig().apiURL}/dags/${opts.name}?remoteNode=${
+      appBarContext.selectedRemoteNode || 'local'
+    }`;
+    console.log('requestId', opts.requestId);
+    const ret = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        action: action,
+        step: step,
+        requestId: opts.requestId,
+      }),
+    });
+    if (ret.ok) {
+      opts?.onSuccess?.();
+    } else {
+      const e = await ret.text();
+      alert(e);
+      opts?.onFailed?.();
+    }
+  };
   return {
     doPost,
   };
