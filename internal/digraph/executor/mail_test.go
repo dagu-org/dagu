c831612d6607196a157966a1fb18d2372616db44
Arky <hitmanarky@gmail.com>
2025-02-10 20:42:49 +0700

    chore: Email attachment testcase (Fixes #812) (#814)

--- internal/digraph/executor/mail_test.go
+++ internal/digraph/executor/mail_test.go
@@ -3,6 +3,7 @@ package executor
 import (
 	"context"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/dagu-org/dagu/internal/digraph"
@@ -12,9 +13,26 @@ import (
 func TestMail(t *testing.T) {
 	t.Parallel()
 
+	// Create temporary directory for test files
+	tmpDir, err := os.MkdirTemp("", "email-test")
+	if err != nil {
+		t.Fatalf("failed to create temp dir: %v", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	// Create a test email attachment
+	attachFile := filepath.Join(tmpDir, "email.txt")
+	content := []byte("Test email")
+
 	os.Setenv("MAIL_SUBJECT", "Test Subject")
+	err = os.WriteFile(attachFile, content, 0644)
+	if err != nil {
+		t.Fatalf("failed to create temp file: %v", err)
+	}
+
 	t.Cleanup(func() {
 		os.Unsetenv("MAIL_SUBJECT")
+		os.Remove(attachFile)
 	})
 
 	t.Run("NewMail", func(t *testing.T) {
@@ -27,10 +45,11 @@ func TestMail(t *testing.T) {
 				step: digraph.Step{
 					ExecutorConfig: digraph.ExecutorConfig{
 						Config: map[string]any{
-							"from":    "test@example.com",
-							"to":      "recipient@example.com",
-							"subject": "Test Subject",
-							"message": "Test Message",
+							"from":        "test@example.com",
+							"to":          "recipient@example.com",
+							"subject":     "Test Subject",
+							"message":     "Test Message",
+							"attachments": attachFile,
 						},
 					},
 				},
@@ -40,10 +59,11 @@ func TestMail(t *testing.T) {
 				step: digraph.Step{
 					ExecutorConfig: digraph.ExecutorConfig{
 						Config: map[string]any{
-							"from":    "test@example.com",
-							"to":      "recipient@example.com",
-							"subject": "${MAIL_SUBJECT}",
-							"message": "Test Message",
+							"from":        "test@example.com",
+							"to":          "recipient@example.com",
+							"subject":     "${MAIL_SUBJECT}",
+							"message":     "Test Message",
+							"attachments": attachFile,
 						},
 					},
 				},
@@ -68,6 +88,7 @@ func TestMail(t *testing.T) {
 				assert.Equal(t, "recipient@example.com", mailExec.cfg.To)
 				assert.Equal(t, "Test Subject", mailExec.cfg.Subject)
 				assert.Equal(t, "Test Message", mailExec.cfg.Message)
+				assert.Equal(t, attachFile, mailExec.cfg.Attachments[0])
 			})
 		}
 	})
