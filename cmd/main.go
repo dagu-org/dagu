dc8672bb31a4174c74c81e1d9f63508ec3684755
Yota Hamada <yohamta@gmail.com>
2025-02-24 17:58:58 +0900

    update command comment

--- cmd/main.go
+++ cmd/main.go
@@ -8,15 +8,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	// version is set at build time
-	version = "0.0.0"
-	rootCmd = &cobra.Command{
-		Use:   build.Slug,
-		Short: "YAML-based DAG scheduling tool.",
-		Long:  `YAML-based DAG scheduling tool.`,
-	}
-)
+var rootCmd = &cobra.Command{
+	Use:   build.Slug,
+	Short: "A compact, portable, and language-agnostic workflow engine.",
+	Long:  `Dagu is a comprehensive workflow orchestration tool that leverages a simple, human-readable YAML syntax to define and execute directed acyclic graphs (DAGs) of tasks. It supports advanced features such as parameterized execution (both positional and named), dry-run simulations, automated retries, and real-time status monitoring. With built-in integration for multiple executors (Docker, HTTP, SSH, mail, etc.) and an intuitive web UI, Dagu streamlines the management of job dependencies, error recovery, and logging across both local and production environments. Its modular architecture and minimal configuration requirements make it an ideal solution for automating complex processes while ensuring flexibility and reliability.`,
+}
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
@@ -25,12 +21,6 @@ func main() {
 }
 
 func init() {
-	build.Version = version
-
-	registerCommands()
-}
-
-func registerCommands() {
 	rootCmd.AddCommand(cmd.CmdStart())
 	rootCmd.AddCommand(cmd.CmdStop())
 	rootCmd.AddCommand(cmd.CmdRestart())
@@ -41,4 +31,8 @@ func registerCommands() {
 	rootCmd.AddCommand(cmd.CmdScheduler())
 	rootCmd.AddCommand(cmd.CmdRetry())
 	rootCmd.AddCommand(cmd.CmdStartAll())
+
+	build.Version = version
 }
+
+var version = "0.0.0"
