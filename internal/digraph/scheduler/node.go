e5a6222379a6af08e84b7df779efab05a9a81b1d
Yota Hamada <yohamta@gmail.com>
2025-03-22 21:34:04 +0900

    avoid trimming space for command output variable

--- internal/digraph/scheduler/node.go
+++ internal/digraph/scheduler/node.go
@@ -8,7 +8,6 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -700,5 +699,5 @@ func (oc *OutputCoordinator) capturedOutput(ctx context.Context) (string, error)
 	if _, err := io.Copy(&buf, oc.outputReader); err != nil {
 		return "", fmt.Errorf("io: failed to read output: %w", err)
 	}
-	return strings.TrimSpace(buf.String()), nil
+	return buf.String(), nil
 }
