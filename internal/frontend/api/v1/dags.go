b71ee6faf627fa61fbab0721c85487050fa6d504
Yota Hamada <yohamta@gmail.com>
2025-04-05 16:19:37 +0900

    wip

--- internal/frontend/api/v1/dags.go
+++ internal/frontend/api/v1/dags.go
@@ -38,7 +38,116 @@ func (a *API) DeleteDAG(ctx context.Context, request api.DeleteDAGRequestObject)
 
 // GetDAGDetails implements api.StrictServerInterface.
 func (a *API) GetDAGDetails(ctx context.Context, request api.GetDAGDetailsRequestObject) (api.GetDAGDetailsResponseObject, error) {
-	panic("unimplemented")
+	name := request.Name
+
+	tab := api.DAGDetailTabStatus
+	if request.Params.Tab != nil {
+		tab = *request.Params.Tab
+	}
+
+	status, err := a.client.GetStatus(ctx, name)
+	if err != nil {
+		return nil, newNotFoundError(api.ErrorCodeNotFound, err)
+	}
+
+	var steps []api.Step
+	for _, step := range status.DAG.Steps {
+		steps = append(steps, toStep(step))
+	}
+
+	handlers := status.DAG.HandlerOn
+
+	handlerOn := api.HandlerOn{}
+	if handlers.Failure != nil {
+		handlerOn.Failure = ptr(toStep(*handlers.Failure))
+	}
+	if handlers.Success != nil {
+		handlerOn.Success = ptr(toStep(*handlers.Success))
+	}
+	if handlers.Cancel != nil {
+		handlerOn.Cancel = ptr(toStep(*handlers.Cancel))
+	}
+	if handlers.Exit != nil {
+		handlerOn.Exit = ptr(toStep(*handlers.Exit))
+	}
+
+	var schedules []api.Schedule
+	for _, s := range status.DAG.Schedule {
+		schedules = append(schedules, api.Schedule{
+			Expression: s.Expression,
+		})
+	}
+
+	var preconditions []api.Precondition
+	for _, p := range status.DAG.Preconditions {
+		preconditions = append(preconditions, toPrecondition(p))
+	}
+
+	dag := status.DAG
+	details := api.DAGDetails{
+		DefaultParams:     dag.DefaultParams,
+		Delay:             int(dag.Delay.Seconds()),
+		Description:       dag.Description,
+		Env:               dag.Env,
+		Group:             dag.Group,
+		HandlerOn:         handlerOn,
+		HistRetentionDays: dag.HistRetentionDays,
+		Location:          dag.Location,
+		LogDir:            dag.LogDir,
+		MaxActiveRuns:     dag.MaxActiveRuns,
+		Name:              dag.Name,
+		Params:            dag.Params,
+		Preconditions:     preconditions,
+		Schedule:          schedules,
+		Steps:             steps,
+		Tags:              dag.Tags,
+	}
+
+	statusDetails := api.DAGStatusFileDetails{
+		DAG:       details,
+		Error:     value(status.ErrorT),
+		File:      status.File,
+		Status:    toStatus(status.Status),
+		Suspended: status.Suspended,
+	}
+
+	if status.Error != nil {
+		statusDetails.Error = status.Error.Error()
+	}
+
+	resp := &api.GetDAGDetails200JSONResponse{
+		Title: status.DAG.Name,
+		DAG:   statusDetails,
+		Tab:   tab,
+	}
+
+	if err != nil {
+		resp.Errors = append(resp.Errors, err.Error())
+	}
+
+	if err := status.DAG.Validate(); err != nil {
+		resp.Errors = append(resp.Errors, err.Error())
+	}
+
+	switch tab {
+	case api.DAGDetailTabStatus:
+		return resp, nil
+
+	case api.DAGDetailTabSpec:
+		return a.processSpecRequest(ctx, name, resp)
+
+	case api.DAGDetailTabHistory:
+		return a.processLogRequest(ctx, resp, dag)
+
+	case api.DAGDetailTabLog:
+		return a.processStepLogRequest(ctx, dag, name, resp)
+
+	case api.DAGDetailTabSchedulerLog:
+		return a.processSchedulerLogRequest(ctx, dag, name, resp)
+
+	default:
+		panic("unreachable")
+	}
 }
 
 // ListDAGs implements api.StrictServerInterface.
@@ -94,7 +203,7 @@ func (a *API) ListDAGs(ctx context.Context, request api.ListDAGsRequestObject) (
 			File:      item.File,
 			Status:    status,
 			Suspended: item.Suspended,
-			DAG:       convertToDAG(item.DAG),
+			DAG:       toDAG(item.DAG),
 		}
 
 		if item.Error != nil {
@@ -129,7 +238,7 @@ func (a *API) SearchDAGs(ctx context.Context, request api.SearchDAGsRequestObjec
 	panic("unimplemented")
 }
 
-func convertToDAG(dag *digraph.DAG) api.DAG {
+func toDAG(dag *digraph.DAG) api.DAG {
 	var schedules []api.Schedule
 	for _, s := range dag.Schedule {
 		schedules = append(schedules, api.Schedule{Expression: s.Expression})
@@ -143,5 +252,89 @@ func convertToDAG(dag *digraph.DAG) api.DAG {
 		DefaultParams: ptr(dag.DefaultParams),
 		Tags:          ptr(dag.Tags),
 		Schedule:      ptr(schedules),
+	}
+}
+
+func toStep(obj digraph.Step) api.Step {
+	var conditions []api.Precondition
+	for _, cond := range obj.Preconditions {
+		conditions = append(conditions, toPrecondition(cond))
+	}
+
+	repeatPolicy := api.RepeatPolicy{
+		Repeat:   ptr(obj.RepeatPolicy.Repeat),
+		Interval: ptr(int(obj.RepeatPolicy.Interval.Seconds())),
+	}
+
+	step := api.Step{
+		Args:          obj.Args,
+		CmdWithArgs:   obj.CmdWithArgs,
+		Command:       obj.Command,
+		Depends:       obj.Depends,
+		Description:   obj.Description,
+		Dir:           obj.Dir,
+		MailOnError:   obj.MailOnError,
+		Name:          obj.Name,
+		Output:        obj.Output,
+		Preconditions: conditions,
+		RepeatPolicy:  repeatPolicy,
+		Script:        obj.Script,
+	}
+
+	if obj.SubWorkflow != nil {
+		step.Run = ptr(obj.SubWorkflow.Name)
+		step.Params = ptr(obj.SubWorkflow.Params)
+	}
+	return step
+}
+
+func toPrecondition(obj digraph.Condition) api.Precondition {
+	return api.Precondition{
+		Condition: ptr(obj.Condition),
+		Expected:  ptr(obj.Expected),
+	}
+}
+
+func toStatus(s persistence.Status) api.DAGStatusDetails {
+	status := api.DAGStatusDetails{
+		Log:        s.Log,
+		Name:       s.Name,
+		Params:     s.Params,
+		Pid:        int(s.PID),
+		RequestId:  s.RequestID,
+		StartedAt:  s.StartedAt,
+		FinishedAt: s.FinishedAt,
+		Status:     api.RunStatus(s.Status),
+		StatusText: api.RunStatusText(s.StatusText),
+	}
+	for _, n := range s.Nodes {
+		status.Nodes = append(status.Nodes, toNode(n))
+	}
+	if s.OnSuccess != nil {
+		status.OnSuccess = toNode(s.OnSuccess)
+	}
+	if s.OnFailure != nil {
+		status.OnFailure = toNode(s.OnFailure)
+	}
+	if s.OnCancel != nil {
+		status.OnCancel = toNode(s.OnCancel)
+	}
+	if s.OnExit != nil {
+		status.OnExit = toNode(s.OnExit)
+	}
+	return status
+}
+
+func toNode(node *persistence.Node) api.Node {
+	return api.Node{
+		DoneCount:  node.DoneCount,
+		Error:      node.Error,
+		FinishedAt: node.FinishedAt,
+		Log:        node.Log,
+		RetryCount: node.RetryCount,
+		StartedAt:  node.StartedAt,
+		Status:     api.NodeStatus(node.Status),
+		StatusText: api.NodeStatusText(node.StatusText),
+		Step:       toStep(node.Step),
 	}
 }
