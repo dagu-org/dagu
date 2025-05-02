9eac36a446307c97dcb299d879f6613217b732cb
halalala222 <1741196223@qq.com>
2025-05-02 15:24:33 +0800

    feat: add git-checkout executor

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/dagu-org/dagu
 
-go 1.23
+go 1.23.0
+
+toolchain go1.24.1
 
 require (
 	github.com/Masterminds/sprig/v3 v3.2.3
@@ -8,6 +10,7 @@ require (
 	github.com/docker/docker v27.4.1+incompatible
 	github.com/fsnotify/fsnotify v1.7.0
 	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-git/go-git/v5 v5.16.0
 	github.com/go-openapi/errors v0.22.0
 	github.com/go-openapi/loads v0.22.0
 	github.com/go-openapi/runtime v0.28.0
@@ -36,7 +39,7 @@ require (
 	github.com/stretchr/testify v1.10.0
 	go.uber.org/goleak v1.3.0
 	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0
-	golang.org/x/text v0.21.0
+	golang.org/x/text v0.24.0
 	gopkg.in/yaml.v2 v2.4.0
 	gotest.tools/gotestsum v1.12.0
 	mvdan.cc/sh/v3 v3.10.0
@@ -45,6 +48,7 @@ require (
 require (
 	4d63.com/gocheckcompilerdirectives v1.2.1 // indirect
 	4d63.com/gochecknoglobals v0.2.1 // indirect
+	dario.cat/mergo v1.0.0 // indirect
 	github.com/4meepo/tagalign v1.3.4 // indirect
 	github.com/Abirdcfly/dupword v0.1.3 // indirect
 	github.com/Antonboom/errname v1.0.0 // indirect
@@ -56,8 +60,9 @@ require (
 	github.com/GaijinEntertainment/go-exhaustruct/v3 v3.3.0 // indirect
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.3.0 // indirect
-	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/OpenPeeDeeP/depguard/v2 v2.2.0 // indirect
+	github.com/ProtonMail/go-crypto v1.1.6 // indirect
 	github.com/alecthomas/go-check-sumtype v0.2.0 // indirect
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20231202071711-9a357b53e9c9 // indirect
@@ -84,8 +89,10 @@ require (
 	github.com/charithe/durationcheck v0.0.10 // indirect
 	github.com/chavacava/garif v0.1.0 // indirect
 	github.com/ckaznocha/intrange v0.2.1 // indirect
+	github.com/cloudflare/circl v1.6.1 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/curioswitch/go-reassign v0.2.0 // indirect
+	github.com/cyphar/filepath-securejoin v0.4.1 // indirect
 	github.com/daixiang0/gci v0.13.5 // indirect
 	github.com/dave/dst v0.27.3 // indirect
 	github.com/denis-tingaikin/go-header v0.5.0 // indirect
@@ -93,6 +100,7 @@ require (
 	github.com/dnephin/pflag v1.0.7 // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/ettle/strcase v0.2.0 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/fatih/structtag v1.2.0 // indirect
@@ -101,6 +109,8 @@ require (
 	github.com/fzipp/gocyclo v0.6.0 // indirect
 	github.com/ghostiam/protogetter v0.3.8 // indirect
 	github.com/go-critic/go-critic v0.11.5 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.6.2 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/analysis v0.23.0 // indirect
@@ -118,7 +128,8 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gofrs/flock v0.12.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golangci/dupl v0.0.0-20180902072040-3e9179ac440a // indirect
 	github.com/golangci/go-printf-func-name v0.1.0 // indirect
 	github.com/golangci/gofmt v0.0.0-20240816233607-d8596aa466a9 // indirect
@@ -127,7 +138,7 @@ require (
 	github.com/golangci/plugin-module-register v0.1.1 // indirect
 	github.com/golangci/revgrep v0.5.3 // indirect
 	github.com/golangci/unconvert v0.0.0-20240309020433-c5143eacb3ed // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/go-github/v66 v66.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
@@ -144,12 +155,14 @@ require (
 	github.com/huandu/xstrings v1.4.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/itchyny/timefmt-go v0.1.5 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/jgautheron/goconst v1.7.1 // indirect
 	github.com/jingyugao/rowserrcheck v1.1.1 // indirect
 	github.com/jjti/go-spancheck v0.6.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/julz/importas v0.1.0 // indirect
 	github.com/karamaru-alpha/copyloopvar v1.1.0 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/kisielk/errcheck v1.8.0 // indirect
 	github.com/kkHAIKE/contextcheck v1.1.5 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
@@ -189,6 +202,7 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/pjbgf/sha1cd v0.3.2 // indirect
 	github.com/polyfloyd/go-errorlint v1.7.0 // indirect
 	github.com/prometheus/client_golang v1.12.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
@@ -200,7 +214,7 @@ require (
 	github.com/quasilyte/regex/syntax v0.0.0-20210819130434-b3f0c404a727 // indirect
 	github.com/quasilyte/stdinfo v0.0.0-20220114132959-f7386bf02567 // indirect
 	github.com/raeperd/recvcheck v0.1.2 // indirect
-	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/ryancurrah/gomodguard v1.3.5 // indirect
 	github.com/ryanrolds/sqlclosecheck v0.5.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
@@ -210,11 +224,13 @@ require (
 	github.com/sashamelentyev/interfacebloat v1.1.0 // indirect
 	github.com/sashamelentyev/usestdlibvars v1.27.0 // indirect
 	github.com/securego/gosec/v2 v2.21.4 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/shazow/go-diff v0.0.0-20160112020656-b6b7b6733b8c // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/sivchari/containedctx v1.0.3 // indirect
 	github.com/sivchari/tenv v1.12.1 // indirect
+	github.com/skeema/knownhosts v1.3.1 // indirect
 	github.com/sonatard/noctx v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/sourcegraph/go-diff v0.7.0 // indirect
@@ -237,6 +253,7 @@ require (
 	github.com/uudashr/gocognit v1.1.3 // indirect
 	github.com/uudashr/iface v1.2.1 // indirect
 	github.com/x-cray/logrus-prefixed-formatter v0.5.2 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/xen0n/gosmopolitan v1.2.2 // indirect
 	github.com/yagipy/maintidx v1.0.0 // indirect
 	github.com/yeya24/promlinter v0.3.0 // indirect
@@ -260,14 +277,15 @@ require (
 	golang.org/x/exp/typeparams v0.0.0-20241108190413-2d47ceb2692f // indirect
 	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/oauth2 v0.24.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/term v0.27.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/term v0.31.0 // indirect
 	golang.org/x/tools v0.27.0 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	honnef.co/go/tools v0.5.1 // indirect
 	mvdan.cc/gofumpt v0.7.0 // indirect
 	mvdan.cc/unparam v0.0.0-20240528143540-8a5130ca722f // indirect
@@ -279,8 +297,8 @@ require (
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/samber/lo v1.38.1
-	golang.org/x/crypto v0.31.0
-	golang.org/x/net v0.32.0
-	golang.org/x/sys v0.28.0
+	golang.org/x/crypto v0.37.0
+	golang.org/x/net v0.39.0
+	golang.org/x/sys v0.32.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/digraph/executor/checkout.go
@@ -0,0 +1,465 @@
+package executor
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/dagu-org/dagu/internal/digraph"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	gitHttp "github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	"github.com/go-git/go-git/v5/storage/filesystem"
+	"github.com/go-viper/mapstructure/v2"
+)
+
+const (
+	gitCheckOutExecutorType = "git-checkout"
+	defaultSSHUser          = "git"
+)
+
+var (
+	errGitCheckoutAuthConfigInvalid = fmt.Errorf("invalid git checkout auth config")
+)
+
+var _ Executor = (*gitCheckout)(nil)
+
+func init() {
+	Register(gitCheckOutExecutorType, newCheckout)
+}
+
+type gitCheckoutExecConfigDefinition struct {
+	Repo     string
+	Ref      string
+	Path     string
+	Depth    int
+	Progress bool
+	Cache    bool
+	Auth     gitCheckoutExecAuthConfigDefinition
+}
+
+func (g *gitCheckoutExecConfigDefinition) getRepoCachePath() string {
+	// https://github.com/dagu-org/dagu.git -> github.com/dagu-org/dagu.git
+	// git@github.com:dagu-org/dagu.git -> github.com/dagu-org/dagu.git
+
+	repo := strings.TrimPrefix(g.Repo, "https://")
+	repo = strings.TrimPrefix(repo, "git@")
+
+	return fmt.Sprintf("~/.cache/dagu/git/%s", repo)
+}
+
+type gitCheckoutExecAuthConfigDefinition struct {
+	Method         string
+	TokenEnv       string
+	UserName       string
+	Password       string
+	SSHUser        string
+	SSHKey         string
+	SSHKeyPassword string
+	SSHAgent       bool
+}
+
+// httpAuthMethod returns http auth method, if auth config is not set, return nil
+func (g *gitCheckoutExecAuthConfigDefinition) httpAuthMethod() (transport.AuthMethod, error) {
+	if g.TokenEnv != "" {
+		g.UserName = g.SSHUser
+		g.Password = os.Getenv(g.TokenEnv)
+	}
+
+	if len(g.UserName) == 0 || len(g.Password) == 0 {
+		return nil, errGitCheckoutAuthConfigInvalid
+	}
+
+	return &gitHttp.BasicAuth{
+		Username: g.UserName,
+		Password: g.Password,
+	}, nil
+}
+
+// sshAuthMethod returns ssh auth method, if auth config is not set, return nil
+func (g *gitCheckoutExecAuthConfigDefinition) sshAuthMethod() (transport.AuthMethod, error) {
+	var (
+		authMethod transport.AuthMethod
+		publicKey  *ssh.PublicKeys
+		err        error
+	)
+
+	if g.SSHAgent {
+		if authMethod, err = ssh.NewSSHAgentAuth(g.SSHUser); err != nil {
+			return nil, fmt.Errorf("failed to create ssh agent auth: %w", err)
+		}
+
+		return authMethod, nil
+	}
+
+	if _, err = os.Stat(g.SSHKey); err != nil {
+		return nil, fmt.Errorf("failed to find ssh key file: %w", err)
+	}
+
+	if publicKey, err = ssh.NewPublicKeysFromFile(g.SSHUser, g.SSHKey, g.SSHKeyPassword); err != nil {
+		return nil, fmt.Errorf("failed to create ssh public keys: %w", err)
+	}
+
+	return publicKey, nil
+}
+
+func (g *gitCheckoutExecAuthConfigDefinition) authMethod(repo string) (transport.AuthMethod, error) {
+	if len(g.SSHUser) == 0 {
+		g.SSHUser = defaultSSHUser
+	}
+
+	// example: https://github.com/dagu-org/dagu.git, use http auth
+	if strings.HasPrefix(repo, "https://") {
+		return g.httpAuthMethod()
+	}
+
+	// example: git@github.com:dagu-org/dagu.git, use ssh auth
+	return g.sshAuthMethod()
+}
+
+type gitCheckoutExecConfig struct {
+	repo          string
+	ref           string
+	path          string
+	depth         int
+	progress      bool
+	cache         bool
+	repoCachePath string
+}
+
+type gitCheckout struct {
+	stdout     io.Writer
+	stderr     io.Writer
+	authMethod transport.AuthMethod
+	config     *gitCheckoutExecConfig
+}
+
+func convertFromDef(def *gitCheckoutExecConfigDefinition, authMethod transport.AuthMethod) *gitCheckout {
+	return &gitCheckout{
+		stdout: os.Stdout,
+		stderr: os.Stderr,
+		config: &gitCheckoutExecConfig{
+			repo:          def.Repo,
+			ref:           def.Ref,
+			path:          def.Path,
+			depth:         def.Depth,
+			progress:      def.Progress,
+			cache:         def.Cache,
+			repoCachePath: def.getRepoCachePath(),
+		},
+		authMethod: authMethod,
+	}
+}
+
+func newCheckout(_ context.Context, step digraph.Step) (Executor, error) {
+	var (
+		def        = &gitCheckoutExecConfigDefinition{}
+		authMethod transport.AuthMethod
+		err        error
+	)
+
+	if err = decodeGitCheckoutConfig(step.ExecutorConfig.Config, def); err != nil {
+		return nil, fmt.Errorf("failed to decode git checkout config: %w", err)
+	}
+
+	if authMethod, err = def.Auth.authMethod(def.Repo); err != nil {
+		return nil, err
+	}
+
+	return convertFromDef(def, authMethod), nil
+}
+
+func decodeGitCheckoutConfig(data map[string]any, config *gitCheckoutExecConfigDefinition) error {
+	var (
+		mapDecoder   *mapstructure.Decoder
+		decodeConfig = &mapstructure.DecoderConfig{
+			Result:           config,
+			WeaklyTypedInput: true,
+		}
+		err error
+	)
+
+	if mapDecoder, err = mapstructure.NewDecoder(decodeConfig); err != nil {
+		return fmt.Errorf("failed to create map decoder: %w", err)
+	}
+
+	if err = mapDecoder.Decode(data); err != nil {
+		return fmt.Errorf("failed to decode git checkout config: %w", err)
+	}
+
+	return nil
+}
+
+func (g *gitCheckout) SetStdout(out io.Writer) {
+	g.stdout = out
+}
+
+func (g *gitCheckout) SetStderr(out io.Writer) {
+	g.stderr = out
+}
+
+func (g *gitCheckout) Kill(_ os.Signal) error {
+	return nil
+}
+
+func (g *gitCheckout) rmWorkPathIfExists() error {
+	var (
+		err error
+	)
+
+	if err = os.RemoveAll(g.config.path); err != nil {
+		return fmt.Errorf("failed to remove %s: %w", g.config.path, err)
+	}
+
+	return nil
+}
+
+// getFetchOptions returns the fetch options
+// ref may be in the form of
+// branch : "refs/heads/main" or "main"
+// tag : "refs/tags/v1.0" or "v1.0"
+// commit : "refs/commits/abc123" or "abc123"
+func (g *gitCheckout) getFetchOptions() (*git.FetchOptions, error) {
+	var (
+		fetchOptions = &git.FetchOptions{
+			RemoteName: git.DefaultRemoteName,
+			RemoteURL:  g.config.repo,
+			Auth:       g.authMethod,
+			Depth:      g.config.depth,
+			RefSpecs: []config.RefSpec{
+				config.RefSpec(fmt.Sprintf("+refs/heads/%s:refs/remotes/origin/%s", g.config.ref, g.config.ref)),
+			},
+		}
+	)
+
+	if g.config.progress {
+		fetchOptions.Progress = g.stdout
+	}
+
+	if len(g.config.ref) == 0 {
+		return nil, fmt.Errorf("ref is empty")
+	}
+
+	if strings.HasPrefix(g.config.ref, "refs/") {
+		fetchOptions.RefSpecs = []config.RefSpec{config.RefSpec(g.config.ref)}
+	}
+
+	if g.isTag() {
+		fetchOptions.RefSpecs = []config.RefSpec{config.RefSpec(fmt.Sprintf("+refs/tags/%s:refs/tags/%s", g.config.ref, g.config.ref))}
+	}
+
+	if g.isHash() {
+		hash := plumbing.NewHash(g.config.ref)
+		fetchOptions.RefSpecs = []config.RefSpec{config.RefSpec(fmt.Sprintf("+%[1]s:%[1]s", hash, hash))}
+		fetchOptions.Tags = git.NoTags
+	}
+
+	return fetchOptions, nil
+}
+
+// initRepo initializes the git repository
+func (g *gitCheckout) initRepo() (*git.Repository, error) {
+	var (
+		repo *git.Repository
+		err  error
+	)
+
+	if repo, err = git.PlainInit(g.config.path, false); err != nil {
+		return nil, fmt.Errorf("failed to init git repository: %w", err)
+	}
+
+	if _, err = repo.CreateRemote(&config.RemoteConfig{
+		Name: git.DefaultRemoteName,
+		URLs: []string{g.config.repo},
+	}); err != nil {
+		return nil, fmt.Errorf("failed to create remote repository: %w", err)
+	}
+
+	return repo, nil
+}
+
+func (g *gitCheckout) setRepoAlternate(repo *git.Repository) error {
+	var (
+		err error
+	)
+
+	storage, ok := repo.Storer.(*filesystem.Storage)
+	if !ok {
+		return fmt.Errorf("unexpected storage type")
+	}
+
+	if err = storage.AddAlternate(g.config.repoCachePath); err != nil {
+		return fmt.Errorf("failed to add alternate: %w", err)
+	}
+
+	return nil
+}
+
+func (g *gitCheckout) getRepo() (*git.Repository, error) {
+	var (
+		repo *git.Repository
+		err  error
+	)
+
+	if repo, err = g.initRepo(); err != nil {
+		return nil, err
+	}
+
+	if !g.config.cache {
+		return repo, nil
+	}
+
+	if err = g.setRepoAlternate(repo); err != nil {
+		return nil, err
+	}
+
+	return repo, nil
+}
+
+func (g *gitCheckout) fetch(ctx context.Context, repo *git.Repository) error {
+	var (
+		fetchOptions *git.FetchOptions
+		err          error
+	)
+
+	if fetchOptions, err = g.getFetchOptions(); err != nil {
+		return fmt.Errorf("failed to get fetch options: %w", err)
+	}
+
+	if err = repo.FetchContext(ctx, fetchOptions); err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
+		return fmt.Errorf("failed to fetch repository: %w", err)
+	}
+
+	return nil
+}
+
+// isTag checks if the ref is a tag or a short tag
+func (g *gitCheckout) isTag() bool {
+	var (
+		tagRegex      = regexp.MustCompile(`^refs/tags/.*$`)
+		shortTagRegex = regexp.MustCompile(`^v(\d+\.)+\d+$`)
+	)
+
+	return tagRegex.MatchString(g.config.ref) || shortTagRegex.MatchString(g.config.ref)
+}
+
+// isHash checks if the ref is a hash
+func (g *gitCheckout) isHash() bool {
+	var (
+		hashRegex = regexp.MustCompile(`^[0-9a-f]{40}$`)
+	)
+
+	return hashRegex.MatchString(g.config.ref)
+}
+
+func (g *gitCheckout) getCheckoutOptions(repo *git.Repository) (*git.CheckoutOptions, error) {
+	var (
+		isHash          = g.isHash()
+		isTag           = g.isTag()
+		checkoutOptions = &git.CheckoutOptions{}
+		revHash         *plumbing.Hash
+		err             error
+	)
+
+	if !isHash && !isTag {
+		checkoutOptions.Force = true
+
+		localBranchReferenceName := plumbing.NewBranchReferenceName(g.config.ref)
+		remoteReferenceName := plumbing.NewRemoteReferenceName(git.DefaultRemoteName, g.config.ref)
+		newReference := plumbing.NewSymbolicReference(localBranchReferenceName, remoteReferenceName)
+
+		if err = repo.Storer.SetReference(newReference); err != nil {
+			return nil, fmt.Errorf("failed to set reference: %w", err)
+		}
+		checkoutOptions.Branch = localBranchReferenceName
+
+		return checkoutOptions, nil
+	}
+
+	if revHash, err = repo.ResolveRevision(plumbing.Revision(g.config.ref)); err != nil {
+		return nil, fmt.Errorf("failed to resolve revision: %w", err)
+	}
+
+	checkoutOptions.Hash = plumbing.NewHash(revHash.String())
+
+	return checkoutOptions, nil
+}
+
+func (g *gitCheckout) checkout(repo *git.Repository) error {
+	var (
+		worktree       *git.Worktree
+		checkoutOption *git.CheckoutOptions
+		err            error
+	)
+
+	if worktree, err = repo.Worktree(); err != nil {
+		return fmt.Errorf("failed to get worktree: %w", err)
+	}
+
+	if checkoutOption, err = g.getCheckoutOptions(repo); err != nil {
+		return err
+	}
+
+	if err = worktree.Checkout(checkoutOption); err != nil {
+		return fmt.Errorf("failed to checkout branch: %w", err)
+	}
+
+	return nil
+}
+
+func (g *gitCheckout) saveCache() error {
+	var (
+		workDir   = os.DirFS(g.config.path)
+		isExisted bool
+		err       error
+	)
+
+	if _, err = os.Stat(g.config.repoCachePath); err == nil {
+		isExisted = true
+	}
+
+	if g.config.cache && !isExisted {
+		if err = os.CopyFS(g.config.repoCachePath, workDir); err != nil {
+			return fmt.Errorf("failed to copy git cache: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (g *gitCheckout) Run(ctx context.Context) error {
+	var (
+		repo *git.Repository
+		err  error
+	)
+
+	if err = g.rmWorkPathIfExists(); err != nil {
+		return err
+	}
+
+	if repo, err = g.getRepo(); err != nil {
+		return err
+	}
+
+	if err = g.fetch(ctx, repo); err != nil {
+		return err
+	}
+
+	if err = g.checkout(repo); err != nil {
+		return err
+	}
+
+	if err = g.saveCache(); err != nil {
+		return err
+	}
+
+	return nil
+}
