4b2ee1bd3f2d54887187a41ac9aaf66a1802e218
Yota Hamada <yohamta@gmail.com>
2025-05-16 15:24:58 +0900

    feat: JobQueue interface definitions

---
+++ internal/models/queue_repo.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"context"
+
+	"github.com/dagu-org/dagu/internal/digraph"
+)
+
+// JobQueueRepository provides an interface for interacting with the underlying database
+// for storing and retrieving queued workflows.
+type JobQueueRepository interface {
+	// Enqueue adds a workflow to the queue
+	Enqueue(ctx context.Context, name string, ref digraph.WorkflowRef) error
+	// Dequeue removes a workflow from the queue
+	Dequeue(ctx context.Context, name string, id string) (QueuedJob, error)
+	// UpdateStatus updates the status of a queued workflow
+	UpdateStatus(ctx context.Context, name string, id string, status JobQueueState) error
+	// Delete deletes a workflow from the queue
+	Delete(ctx context.Context, name string, id string) error
+}
+
+// QueuedJob represents a workflow that is in the queue for execution
+type QueuedJob interface {
+	// ID returns the ID of the queued workflow
+	ID() string
+	// WorkflowRef returns the reference of the queued workflow
+	WorkflowRef() digraph.WorkflowRef // Status returns the status of the queued workflow
+	// Status returns the status of the queued workflow
+	Status() JobQueueState
+}
+
+// JobQueueState represents the status of a queued workflow
+type JobQueueState int
+
+const (
+	// QueueStatusNone indicates that the workflow is not in the queue
+	QueueStatusNone JobQueueState = iota
+	// QueueStatusQueued indicates that the workflow is in the queue
+	QueueStatusQueued
+	// QueueStatusRunning indicates that the workflow is currently running
+	QueueStatusRunning
+	// QueueStatusCompleted indicates that the workflow has completed
+	QueueStatusCompleted
+)
