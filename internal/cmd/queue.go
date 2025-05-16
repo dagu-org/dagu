a22dc658953a4015ac779b3d524113644926dbb5
Yota Hamada <yohamta@gmail.com>
2025-05-16 14:18:23 +0900

    feat: add skelton for queue command

---
+++ internal/cmd/queue.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func CmdQueue() *cobra.Command {
+	return NewCommand(
+		&cobra.Command{
+			Use:   "queue [flags]",
+			Short: "Start the queue server for workflow execution",
+			Long:  `Launch the queue process.`,
+		}, queueFlags, runQueue,
+	)
+}
+
+var queueFlags = []commandLineFlag{}
+
+func runQueue(ctx *Context, _ []string) error {
+	panic("unimplemented")
+}
