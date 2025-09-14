aa3f4aec220165775b6e46438f6ef781f92a4b5c
Yota Hamada <yohamta@gmail.com>
2025-09-14 23:30:57 +0900

    go fmt

--- internal/digraph/builder.go
+++ internal/digraph/builder.go
@@ -1,8 +1,8 @@
 package digraph
 
 import (
-    "errors"
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -140,20 +140,20 @@ func build(ctx BuildContext, spec *definition) (*DAG, error) {
 	}
 
 	var errs ErrorList
-    for _, builder := range builderRegistry {
-        if !builder.metadata && ctx.opts.OnlyMetadata {
-            continue
-        }
-        if err := builder.fn(ctx, spec, dag); err != nil {
-            // Avoid duplicating field prefixes like "field 'steps': field 'steps': ..."
-            var le *LoadError
-            if errors.As(err, &le) && le.Field == builder.name {
-                errs.Add(err)
-            } else {
-                errs.Add(wrapError(builder.name, nil, err))
-            }
-        }
-    }
+	for _, builder := range builderRegistry {
+		if !builder.metadata && ctx.opts.OnlyMetadata {
+			continue
+		}
+		if err := builder.fn(ctx, spec, dag); err != nil {
+			// Avoid duplicating field prefixes like "field 'steps': field 'steps': ..."
+			var le *LoadError
+			if errors.As(err, &le) && le.Field == builder.name {
+				errs.Add(err)
+			} else {
+				errs.Add(wrapError(builder.name, nil, err))
+			}
+		}
+	}
 
 	if len(errs) > 0 {
 		if ctx.opts.AllowBuildErrors {
