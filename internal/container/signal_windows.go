23d48f7f9784dd60e22e0974165499d1da3f306d
Yota Hamada <yohamta@gmail.com>
2025-09-27 23:57:08 +0900

    multi platform support

--- internal/container/signal_windows.go
+++ internal/container/signal_windows.go
@@ -6,18 +6,11 @@ import "syscall"
 
 var signalMap = map[syscall.Signal]string{
 	syscall.SIGABRT: "SIGABRT",
-	syscall.SIGALRM: "SIGALRM",
-	syscall.SIGBUS:  "SIGBUS",
 	syscall.SIGFPE:  "SIGFPE",
-	syscall.SIGHUP:  "SIGHUP",
 	syscall.SIGILL:  "SIGILL",
 	syscall.SIGINT:  "SIGINT",
-	syscall.SIGKILL: "SIGKILL",
-	syscall.SIGPIPE: "SIGPIPE",
-	syscall.SIGQUIT: "SIGQUIT",
 	syscall.SIGSEGV: "SIGSEGV",
 	syscall.SIGTERM: "SIGTERM",
-	syscall.SIGTRAP: "SIGTRAP",
 }
 
 func signalName(sig syscall.Signal) string {
