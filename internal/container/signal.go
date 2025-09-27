bde6435c4bfa31d42726be5c9523980dbcd94408
Yota Hamada <yohamta@gmail.com>
2025-09-27 23:54:38 +0900

    multi platform support

--- internal/container/signal.go
+++ internal/container/signal.go
@@ -2,24 +2,8 @@ package container
 
 import "syscall"
 
-var signalMap = map[syscall.Signal]string{
-	syscall.SIGABRT: "SIGABRT",
-	syscall.SIGALRM: "SIGALRM",
-	syscall.SIGBUS:  "SIGBUS",
-	syscall.SIGFPE:  "SIGFPE",
-	syscall.SIGHUP:  "SIGHUP",
-	syscall.SIGILL:  "SIGILL",
-	syscall.SIGINT:  "SIGINT",
-	syscall.SIGKILL: "SIGKILL",
-	syscall.SIGPIPE: "SIGPIPE",
-	syscall.SIGQUIT: "SIGQUIT",
-	syscall.SIGSEGV: "SIGSEGV",
-	syscall.SIGTERM: "SIGTERM",
-	syscall.SIGTRAP: "SIGTRAP",
-}
-
 func GetSignalName(sig syscall.Signal) string {
-	if name, ok := signalMap[sig]; ok {
+	if name := signalName(sig); name != "" {
 		return name
 	}
 	return "SIGKILL"
---
+++ internal/container/signal_windows.go
@@ -0,0 +1,28 @@
+//go:build windows
+
+package container
+
+import "syscall"
+
+var signalMap = map[syscall.Signal]string{
+	syscall.SIGABRT: "SIGABRT",
+	syscall.SIGALRM: "SIGALRM",
+	syscall.SIGBUS:  "SIGBUS",
+	syscall.SIGFPE:  "SIGFPE",
+	syscall.SIGHUP:  "SIGHUP",
+	syscall.SIGILL:  "SIGILL",
+	syscall.SIGINT:  "SIGINT",
+	syscall.SIGKILL: "SIGKILL",
+	syscall.SIGPIPE: "SIGPIPE",
+	syscall.SIGQUIT: "SIGQUIT",
+	syscall.SIGSEGV: "SIGSEGV",
+	syscall.SIGTERM: "SIGTERM",
+	syscall.SIGTRAP: "SIGTRAP",
+}
+
+func signalName(sig syscall.Signal) string {
+	if name, ok := signalMap[sig]; ok {
+		return name
+	}
+	return ""
+}
