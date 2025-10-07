76a9f8cc07abe37b8bcbb589ebdbb5f057538273
Yota Hamada <yohamta@gmail.com>
2025-10-07 22:49:22 +0900

    wip

--- internal/config/context.go
+++ internal/config/context.go
@@ -2,11 +2,11 @@ package config
 
 import (
 	"context"
-
-	"github.com/dagu-org/dagu/internal/logger"
 )
 
 type configKey struct{}
+
+const warningNoConfigInContext = "Configuration not found in context, returning empty config"
 
 // WithConfig creates a new context with the provided configuration.
 func WithConfig(ctx context.Context, cfg *Config) context.Context {
@@ -18,6 +18,6 @@ func GetConfig(ctx context.Context) *Config {
 	if cfg, ok := ctx.Value(configKey{}).(*Config); ok {
 		return cfg
 	}
-	logger.Warn(ctx, "Configuration not found in context, returning empty config")
-	return &Config{}
+	warnings := []string{warningNoConfigInContext}
+	return &Config{Warnings: warnings}
 }
