2f0ed0fb47ca5b1ad794a81eb8fffd22ab70f002
Yota Hamada <yohamta@gmail.com>
2025-10-20 00:41:35 +0900

    add init function import

--- internal/runtime/builtin/builtin.go
+++ internal/runtime/builtin/builtin.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/command"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/dag"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/docker"
+	_ "github.com/dagu-org/dagu/internal/runtime/builtin/gha"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/http"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/jq"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/mail"
