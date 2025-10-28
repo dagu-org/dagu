ef79d8c8fa72f8efac2a952a9e0da4b317409cab
Yota Hamada <yohamta@gmail.com>
2025-10-28 23:50:15 +0900

    chore: Remove docs to move out from the main repo

--- docs/.vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
-
-export default {
-  extends: DefaultTheme,
-  enhanceApp({ app }) {
-    // Register custom components here if needed
-  }
-}
