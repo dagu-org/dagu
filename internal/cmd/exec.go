0983b7cea0c763e01a7abf84efa46e55b92a50de
Yota Hamada <yohamta@gmail.com>
2025-11-24 05:01:19 +0900

    fix style

--- internal/cmd/exec.go
+++ internal/cmd/exec.go
@@ -216,13 +216,23 @@ func runExec(ctx *Context, args []string) error {
 	dagRunRef := execution.NewDAGRunRef(dag.Name, runID)
 
 	if !queueDisabled && len(workerLabels) > 0 {
-		logger.Info(ctx, "Queueing inline dag-run for distributed execution", tag.DAG(dag.Name), tag.RunID(runID), slog.Any("worker-selector", workerLabels), tag.Command(strings.Join(args, " ")))
+		logger.Info(ctx, "Queueing inline dag-run for distributed execution",
+			tag.DAG(dag.Name),
+			tag.RunID(runID),
+			slog.Any("worker-selector", workerLabels),
+			tag.Command(strings.Join(args, " ")),
+		)
 		dag.Location = ""
 		return enqueueDAGRun(ctx, dag, runID)
 	}
 
 	if !queueDisabled && dag.Queue != "" {
-		logger.Info(ctx, "Queueing inline dag-run", tag.DAG(dag.Name), tag.Queue(dag.Queue), tag.RunID(runID), tag.Command(strings.Join(args, " ")))
+		logger.Info(ctx, "Queueing inline dag-run",
+			tag.DAG(dag.Name),
+			tag.Queue(dag.Queue),
+			tag.RunID(runID),
+			tag.Command(strings.Join(args, " ")),
+		)
 		dag.Location = ""
 		return enqueueDAGRun(ctx, dag, runID)
 	}
@@ -232,7 +242,11 @@ func runExec(ctx *Context, args []string) error {
 		return fmt.Errorf("dag-run ID %s already exists for DAG %s", runID, dag.Name)
 	}
 
-	logger.Info(ctx, "Executing inline dag-run", tag.DAG(dag.Name), tag.Command(strings.Join(args, " ")), tag.RunID(runID))
+	logger.Info(ctx, "Executing inline dag-run",
+		tag.DAG(dag.Name),
+		tag.Command(strings.Join(args, " ")),
+		tag.RunID(runID),
+	)
 
 	err = tryExecuteDAG(ctx, dag, runID, dagRunRef, false)
 	if errors.Is(err, errMaxRunReached) && !queueDisabled {
