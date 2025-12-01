d1307deda556fc0d7c562853e8204cdb1ccfd161
Simon Petit <nomisp96@hotmail.fr>
2025-12-01 17:22:04 +0000

    wip mcp server functions

--- internal/service/mcpserver/mcpserver.go
+++ internal/service/mcpserver/mcpserver.go
@@ -2,28 +2,98 @@ package mcpserver
 
 import (
 	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync/atomic"
+	"time"
 
+	"github.com/dagu-org/dagu/internal/common/config"
+	"github.com/dagu-org/dagu/internal/common/dirlock"
+	"github.com/dagu-org/dagu/internal/common/logger"
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
-func New(ctx context.Context) *mcp.Server {
+type MCPServer struct {
+	server   *mcp.Server
+	logDir   string
+	running  atomic.Bool
+	config   *config.Config
+	dirLock  dirlock.DirLock
+	location *time.Location
+}
+
+func New(cfg *config.Config) *MCPServer {
+
+	timeLoc := cfg.Global.Location
+	if timeLoc == nil {
+		timeLoc = time.Local
+	}
+	lockOpts := &dirlock.LockOptions{
+		StaleThreshold: cfg.Scheduler.LockStaleThreshold,
+		RetryInterval:  cfg.Scheduler.LockRetryInterval,
+	}
+	lockDir := filepath.Join(cfg.Paths.DataDir, "mcpserver", "locks")
+	dirLock := dirlock.New(lockDir, lockOpts)
+
 	s := mcp.NewServer(&mcp.Implementation{Name: "server", Version: "v0.0.1"}, nil)
 
 	s.AddTool(&mcp.Tool{
-		Name: "List DAGs",
-	}, func(ctx context.Context, ctr *mcp.CallToolRequest) (*mcp.CallToolResult, error) {})
+		Name:        "List DAGs",
+		Description: "List all the Direct Acyclic Graphs in the server",
+	}, listDags)
 
-	//s.AddTool(&mcp.Tool{
-	// 	Name: "Execute DAG",
-	//}, func(ctx context.Context, ctr *mcp.CallToolRequest) (*mcp.CallToolResult, error) {})
+	s.AddTool(&mcp.Tool{
+		Name:        "Execute DAG",
+		Description: "Execute a specific Workflow",
+	}, executeDag)
 
-	//s.AddTool(&mcp.Tool{
-	//	Name: "Create DAG",
-	//}, func(ctx context.Context, ctr *mcp.CallToolRequest) (*mcp.CallToolResult, error) {})
+	s.AddTool(&mcp.Tool{
+		Name:        "Create DAG",
+		Description: "Create a new workflow",
+	}, createDag)
 
-	return s
+	return &MCPServer{
+		server:   s,
+		logDir:   cfg.Paths.LogDir,
+		location: timeLoc,
+		dirLock:  dirLock,
+	}
 }
 
-func Start(ctx context.Context, s *mcp.Server) {
-	s.Run(ctx)
+func listDags(ctx context.Context, ctr *mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	host := os.Getenv("DAGU_HOST")
+	port := os.Getenv("DAGU_PORT")
+	api_base_url := os.Getenv("DAGU_API_BASE_URL")
+
+	_, err := http.Get(fmt.Sprintf("http://%s:%s/%s/dags", host, port, api_base_url))
+
+	if err != nil {
+		return nil, err
+	}
+	return &mcp.CallToolResult{}, nil
+}
+
+func executeDag(ctx context.Context, ctr *mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	return &mcp.CallToolResult{}, nil
+}
+
+func createDag(ctx context.Context, ctr *mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	return &mcp.CallToolResult{}, nil
+}
+
+func (s *MCPServer) Start(ctx context.Context) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Acquire directory lock first to prevent multiple mcp server instances
+	logger.Info(ctx, "Waiting to acquire mcp server lock")
+	if err := s.dirLock.Lock(ctx); err != nil {
+		return fmt.Errorf("failed to acquire mcp server lock: %w", err)
+	}
+
+	logger.Info(ctx, "Acquired scheduler lock")
+
+	return nil
 }
