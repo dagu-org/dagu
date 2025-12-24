6ba787a804552600b57c0defff7e0531942d9b1c
Yota Hamada <yohamta@gmail.com>
2025-12-24 12:52:22 +0900

    add tab to dag definitions

---
+++ ui/src/components/TabBar.tsx
@@ -0,0 +1,67 @@
+import * as TabsPrimitive from '@radix-ui/react-tabs';
+import { Plus, X } from 'lucide-react';
+import React from 'react';
+import { useTabContext } from '../contexts/TabContext';
+import './TabBar.css';
+
+interface TabBarProps {
+  onAddTab?: () => void;
+}
+
+function TabBar({ onAddTab }: TabBarProps) {
+  const { tabs, activeTabId, setActiveTab, closeTab } = useTabContext();
+
+  if (tabs.length === 0) {
+    return null;
+  }
+
+  return (
+    <TabsPrimitive.Root
+      value={activeTabId || ''}
+      onValueChange={setActiveTab}
+    >
+      <div className="tab-bar-wrapper">
+        <TabsPrimitive.List className="tab-list">
+          {tabs.map((tab) => (
+            <TabsPrimitive.Trigger
+              key={tab.id}
+              value={tab.id}
+              className="tab-trigger group"
+            >
+              <span className="tab-title">{tab.title}</span>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  closeTab(tab.id);
+                }}
+                className="tab-close"
+                title="Close"
+              >
+                <X className="h-3 w-3" />
+              </button>
+              {/* Left curve */}
+              <svg className="tab-curve tab-curve-left" width="8" height="8" viewBox="0 0 8 8">
+                <path d="M8 0 Q8 8 0 8 L8 8 Z" />
+              </svg>
+              {/* Right curve */}
+              <svg className="tab-curve tab-curve-right" width="8" height="8" viewBox="0 0 8 8">
+                <path d="M0 0 Q0 8 8 8 L0 8 Z" />
+              </svg>
+            </TabsPrimitive.Trigger>
+          ))}
+          {onAddTab && (
+            <button
+              onClick={onAddTab}
+              className="tab-add"
+              title="New tab"
+            >
+              <Plus className="h-4 w-4" />
+            </button>
+          )}
+        </TabsPrimitive.List>
+      </div>
+    </TabsPrimitive.Root>
+  );
+}
+
+export default TabBar;
