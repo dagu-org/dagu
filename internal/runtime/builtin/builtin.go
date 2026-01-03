21df95c8e280636f43351f727a32ab6248dc6c38
Yota Hamada <yohamta@gmail.com>
2026-01-03 14:29:46 +0900

    go fmt

--- internal/runtime/builtin/builtin.go
+++ internal/runtime/builtin/builtin.go
@@ -2,13 +2,13 @@ package builtin
 
 import (
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/archive"
+	_ "github.com/dagu-org/dagu/internal/runtime/builtin/chat"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/command"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/dag"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/docker"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/gha"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/http"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/jq"
-	_ "github.com/dagu-org/dagu/internal/runtime/builtin/chat"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/mail"
 	_ "github.com/dagu-org/dagu/internal/runtime/builtin/ssh"
 )
